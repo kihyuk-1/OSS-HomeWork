01392a7dec271163bc8674c8a4de1efff14a60a0
kihyuk-1 <zjaosej12@naver.com>
2024-12-01 17:52:39 +0900

    Add files via upload

---
+++ 자바프로그래밍/7장/B.java
@@ -0,0 +1,13 @@
+package ch연습용;
+
+public class B {
+
+	
+	//메소드 선언
+	public void method() {
+		A a = new A();
+		
+		a.field = "value";
+		a.method();
+	}
+}
---
+++ 자바프로그래밍/7장/Car1.java
@@ -0,0 +1,17 @@
+package ch연습용;
+
+public class Car1 {
+
+	// 필드 선언
+	public Tire tire;
+	
+	// 메소드 선언
+	
+	public void run() {
+		
+		//tire 필드에 대입된 객체의 roll() 메소드 호출
+	
+		tire.roll();
+	}
+
+}
